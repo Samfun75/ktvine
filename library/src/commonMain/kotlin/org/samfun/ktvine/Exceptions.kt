afb3f47880b90142ceb22aef4ca7e30015c29e13
Samfun <38332931+Samfun75@users.noreply.github.com>
2025-09-25 12:11:23 +0300

    feat: add protobuf, types and util functions

---
+++ library/src/commonMain/kotlin/org/samfun/ktvine/Exceptions.kt
@@ -0,0 +1,12 @@
+package org.samfun.ktvine
+
+open class KtvineException(message: String) : Exception(message)
+
+class TooManySessionsException(message: String) : KtvineException(message)
+class InvalidSessionException(message: String) : KtvineException(message)
+class DecodeException(message: String) : KtvineException(message)
+class SignatureMismatchException(message: String) : KtvineException(message)
+class InvalidInitDataException(message: String) : KtvineException(message)
+class InvalidLicenseTypeException(message: String) : KtvineException(message)
+class ValueException(message:String): KtvineException(message)
+class InvalidBoxException(message:String): KtvineException(message)
---
+++ library/src/commonMain/kotlin/org/samfun/ktvine/Types.kt
@@ -0,0 +1,5 @@
+package org.samfun.ktvine
+
+import org.mp4parser.boxes.iso23001.part7.ProtectionSystemSpecificHeaderBox
+
+typealias PsshBox = ProtectionSystemSpecificHeaderBox
