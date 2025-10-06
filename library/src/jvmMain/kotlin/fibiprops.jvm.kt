10ee23bad353d3e13b4c650019795b57067c63e3
Samfun <38332931+Samfun75@users.noreply.github.com>
2025-10-06 16:23:15 +0300

    chore: remove demo fibonaci code

--- library/src/jvmMain/kotlin/fibiprops.jvm.kt
+++
@@ -1,4 +0,0 @@
-package org.samfun.ktvine
-
-actual val firstElement: Int = 2
-actual val secondElement: Int = 3
--- library/src/linuxX64Main/kotlin/fibiprops.linuxX64.kt
+++
@@ -1,4 +0,0 @@
-package org.samfun.ktvine
-
-actual val firstElement: Int = 3
-actual val secondElement: Int = 5
